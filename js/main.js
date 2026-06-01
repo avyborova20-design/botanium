3dc7c9b7bae5fcad66042f4aaed42f29b4975935
avyborova20-design <avyborova20@gmail.com>
2026-06-01 22:14:48 +0800

    Добавление навигационного меню и адаптивной темы оформления

---
+++ js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+    
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        if (currentTheme === 'dark') {
+            themeToggleBtn.textContent = 'Светлая тема';
+        }
+    }
+
+    themeToggleBtn.addEventListener('click', () => {
+        let theme = document.documentElement.getAttribute('data-theme');
+        
+        if (theme === 'dark') {
+            document.documentElement.removeAttribute('data-theme');
+            localStorage.setItem('theme', 'light');
+            themeToggleBtn.textContent = 'Сменить тему';
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            themeToggleBtn.textContent = 'Светлая тема';
+        }
+    });
+});
